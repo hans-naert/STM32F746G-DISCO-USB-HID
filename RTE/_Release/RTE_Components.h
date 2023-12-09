aed85e76d54e32509168319b3bdf53eab6b8cc1d
Hans Naert <hans.naert@vives.be>
2023-12-09 21:34:57 +0100

    Generate USB HID example with the pack installer

---
+++ RTE/_Release/RTE_Components.h
@@ -0,0 +1,61 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'HID' 
+ * Target:  'Release' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f7xx.h"
+
+/* ARM::CMSIS:RTOS2:Keil RTX5&Library@5.5.4 */
+#define RTE_CMSIS_RTOS2                 /* CMSIS-RTOS2 */
+        #define RTE_CMSIS_RTOS2_RTX5            /* CMSIS-RTOS2 Keil RTX5 */
+/* Keil::CMSIS Driver:USB Device:Full-speed@1.13.0 */
+#define RTE_Drivers_USBD0               /* Driver USBD0 */
+/* Keil::CMSIS Driver:USB Device:High-speed@1.19.0 */
+#define RTE_Drivers_USBD1               /* Driver USBD1 */
+/* Keil::Device:STM32Cube Framework:Classic@1.3.0 */
+#define RTE_DEVICE_FRAMEWORK_CLASSIC
+/* Keil::Device:STM32Cube HAL:Common@1.3.0 */
+#define RTE_DEVICE_HAL_COMMON
+/* Keil::Device:STM32Cube HAL:Cortex@1.3.0 */
+#define RTE_DEVICE_HAL_CORTEX
+/* Keil::Device:STM32Cube HAL:DMA@1.3.0 */
+#define RTE_DEVICE_HAL_DMA
+/* Keil::Device:STM32Cube HAL:GPIO@1.3.0 */
+#define RTE_DEVICE_HAL_GPIO
+/* Keil::Device:STM32Cube HAL:LTDC@1.3.0 */
+#define RTE_DEVICE_HAL_LTDC
+/* Keil::Device:STM32Cube HAL:PWR@1.3.0 */
+#define RTE_DEVICE_HAL_PWR
+/* Keil::Device:STM32Cube HAL:RCC@1.3.0 */
+#define RTE_DEVICE_HAL_RCC
+/* Keil::Device:STM32Cube HAL:SDRAM@1.3.0 */
+#define RTE_DEVICE_HAL_SDRAM
+/* Keil::Device:STM32Cube HAL:SRAM@1.3.0 */
+#define RTE_DEVICE_HAL_SRAM
+/* Keil::Device:Startup@1.2.5 */
+#define RTE_DEVICE_STARTUP_STM32F7XX    /* Device Startup for STM32F7 */
+/* Keil::USB&MDK-Plus:CORE&Release@6.16.1 */
+#define RTE_USB_Core                    /* USB Core */
+          #define RTE_USB_Core_Release            /* USB Core Release Version */
+/* Keil::USB&MDK-Plus:Device:HID@6.16.1 */
+#define RTE_USB_Device_HID_0            /* USB Device HID instance 0 */
+#define RTE_USB_Device_HID_1            /* USB Device HID instance 1 */
+
+/* Keil::USB&MDK-Plus:Device@6.16.1 */
+#define RTE_USB_Device_0                /* USB Device 0 */
+#define RTE_USB_Device_1                /* USB Device 1 */
+
+
+
+#endif /* RTE_COMPONENTS_H */
